e5e4862931e5d16878855b29168922fec24ff45c
ShaafiyaShaik <skshaafiya@gmail.com>
2024-11-10 15:24:58 +0530

    Update Login.jsx

--- frontend/vite-project/src/Login.jsx
+++ frontend/vite-project/src/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
   
-    const response = await fetch('https://mern-vercell-c62q.vercel.app/login', {
+    const response = await fetch('http://localhost:3000/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(loginData),
