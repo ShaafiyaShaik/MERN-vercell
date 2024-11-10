e2cdefaa5a46c77eeb16dd4ca21c54be68c11ad5
ShaafiyaShaik <skshaafiya@gmail.com>
2024-11-10 15:17:22 +0530

    Update FormSection.jsx

--- frontend/vite-project/src/FormSection.jsx
+++ frontend/vite-project/src/FormSection.jsx
@@ -21,7 +21,7 @@ function FormSection() {
     e.preventDefault(); // Prevent default form submission
   
     try {
-      const response = await fetch('http://localhost:3000/register', {
+      const response = await fetch('https://mern-vercell-c62q.vercel.app/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
